c2b589ed87257e7bef7a6fbd7bbdc27c9f102a43
Doug Krieger <doug@debutdigital.com>
2018-11-01 02:45:50 -0400

    set up REST api scaffolding

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	// "errors"
 	"fmt"
-	"github.com/vanng822/go-solr/solr"
+	// "github.com/vanng822/go-solr/solr"
 	"gopkg.in/urfave/cli.v1"
 	"gopkg.in/urfave/cli.v1/altsrc"
+	"net/http"
 	"os"
 )
 
@@ -36,14 +38,50 @@ func main() {
 		}),
 	}
 
-	app.Before = altsrc.InitInputSourceWithContext(app.Flags, altsrc.NewYamlSourceFromFlagFunc("config"))
+	// app.Before = altsrc.InitInputSourceWithContext(app.Flags, altsrc.NewYamlSourceFromFlagFunc("config"))
+	app.Before = func(c *cli.Context) error {
+		if _, err := os.Stat(c.String("config")); os.IsNotExist(err) {
+			// return errors.New("foobar")
+			return nil
+		}
+		_, err := altsrc.NewYamlSourceFromFlagFunc("config")(c)
+		return err
+	}
 
 	app.Action = func(c *cli.Context) error {
-		si, _ := solr.NewSolrInterface(fmt.Sprintf(
-			"http://%s:%d", c.String("solr.host"),
-			c.Int("solr.port")),
-			c.String("solr.collection"))
-		fmt.Printf("Solr Client Instance: %v", si)
+		// si, _ := solr.NewSolrInterface(fmt.Sprintf(
+		// 	"http://%s:%d", c.String("solr.host"),
+		// 	c.Int("solr.port")),
+		// 	c.String("solr.collection"))
+		// fmt.Printf("Solr Client Instance: %v", si)
+
+		http.HandleFunc("/complete", func(w http.ResponseWriter, req *http.Request) {
+			switch req.Method {
+			case http.MethodGet:
+				if err := req.ParseForm(); err != nil {
+					panic(err)
+				}
+				q := req.Form["q"]
+				query := ""
+				if len(q) > 0 {
+					query = q[0]
+				}
+				// w.Write([]byte(fmt.Sprintf("req: %v\n", req)))
+				w.Write([]byte(fmt.Sprintf("Query: %s\n", query)))
+			default:
+				msg := fmt.Sprintf("Invalid Method: Expecting GET, received %v\n", req.Method)
+				code := http.StatusMethodNotAllowed
+				w.WriteHeader(code)
+				if len, err := w.Write([]byte(msg)); err != nil {
+					fmt.Printf("resp len: %v; err: %v\n", len, err)
+				}
+			}
+		})
+
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			panic(err)
+		}
+
 		return nil
 	}
 
